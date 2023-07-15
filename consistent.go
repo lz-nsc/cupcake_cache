a936d3e63ec636fcbee07592d8a65c4d1c5168e1
Lynn Zhou <lynnreload@163.com>
2023-07-15 21:47:44 +0800

    implement consistent hashing

---
+++ consistent.go
@@ -0,0 +1,52 @@
+package cupcake_cache
+
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+type HashFunc func(data []byte) uint32
+
+type Manager struct {
+	hashFn   HashFunc
+	replicas int // number or virtual node for each node
+	nodes    []int
+	hashMap  map[int]string
+}
+
+func NewManager(replicas int, hashFn HashFunc) *Manager {
+	if hashFn == nil {
+		hashFn = crc32.ChecksumIEEE
+	}
+
+	return &Manager{
+		hashFn:   hashFn,
+		replicas: replicas,
+		nodes:    make([]int, 0),
+		hashMap:  map[int]string{},
+	}
+}
+
+func (m *Manager) AddNodes(nodes ...string) {
+	for _, name := range nodes {
+		for i := 0; i < m.replicas; i++ {
+			hash := m.hashFn([]byte(strconv.Itoa(i) + name))
+			m.nodes = append(m.nodes, int(hash))
+			m.hashMap[int(hash)] = name
+		}
+	}
+	sort.Ints(m.nodes)
+}
+
+func (m *Manager) GetNode(key string) string {
+	if len(m.nodes) == 0 {
+		return ""
+	}
+	hash := m.hashFn([]byte(key))
+	nodeHash := sort.Search(len(m.nodes), func(i int) bool {
+		return m.nodes[i] >= int(hash)
+	})
+
+	return m.hashMap[m.nodes[nodeHash%len(m.nodes)]]
+}
---
+++ consistent_test.go
@@ -0,0 +1,37 @@
+package cupcake_cache
+
+import (
+	"strconv"
+	"testing"
+)
+
+func TestConsistent(t *testing.T) {
+	// Use hash function that the result of test case can be predict
+	mgr := NewManager(3, func(node []byte) uint32 {
+		hash, _ := strconv.Atoi(string(node))
+		return uint32(hash)
+	})
+
+	// virtual nodes: "1":"1","11","20";"10":"10","110","210";"30":"30","130","230"
+	mgr.AddNodes("1", "10", "30")
+
+	testCases := map[string]string{
+		"350": "1",
+		"12":  "1",
+		"100": "10",
+		"111": "30",
+	}
+
+	for key, res := range testCases {
+		node := mgr.GetNode(key)
+		if res != node {
+			t.Errorf("failed to get node, want: %s, got: %s", res, node)
+		}
+	}
+	// virtual nodes: "15":"115","215","30"
+	mgr.AddNodes("15")
+	if node := mgr.GetNode("111"); node != "15" {
+		t.Errorf("failed to get node after added new node, want: 15, got: %s", node)
+	}
+
+}
